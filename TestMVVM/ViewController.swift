a631612f911b29e8707556a32f773a30c6312827
Alexander Pechinkin <pechinkin04@yandex.ru>
2024-05-04 11:06:00 +0400

    Initial Commit

---
+++ TestMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMVVM
+//
+//  Created by Александр Печинкин on 04.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
